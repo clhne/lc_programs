9275f768e2c890a1d39cae08ddece68ec5418992
lucl1 <luchenglin@leadcoretech.com>
2018-02-24 13:45:05 +0800

    [modify] diff interval of atan & atan2

--- C++/atan/atan_demo.cpp
+++ C++/atan/atan_demo.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <errno.h>
+#define PI 3.14159265
+#define cov (180 / PI)
 using namespace std;
 
 int main(int ac, char* av[])
@@ -14,12 +16,17 @@ int main(int ac, char* av[])
     }
     x = atof(av[1]);
     theta = atan(x);
-    printf("Arctangent of %f / %f: %f\n",y, x, theta);
+    printf("Arctangent of %f / %f: %f\n",1.0, x, cov*theta);
     y = atof(av[2]);
     theta = atan2(y, x);
     printf("Arctangent of %f / %f: %f\n",y, x, theta);
+    printf("Arctangent of 1.0/0 : %f\n",cov*atan2(1,0));    //atan2中第二个参数可以为0，atan不能；atan2 in the interval [-pi,+pi], atan [-pi/2,+pi/2]
+    
+    printf("atan2(-1/1) : %f\n",cov*atan2(1.0,-1));
+    printf("atan(-1/1) : %f\n",cov*atan(-1));
+    //printf("atan 0 : %f\n",atan(1.0/0));
 
-    system("pause");
+    //system("pause");
 
     return 0;
 }
--- C++/atan/run.bat
+++ C++/atan/run.bat
@@ -1,4 +1,4 @@
 @echo off
 g++ -g atan_demo.cpp -o atan_demo
-atan_demo.exe 1 2
+atan_demo.exe 1 20000000000
 pause
