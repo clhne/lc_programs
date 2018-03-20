e6a9c8670b4d28387f3f39ee895e134386c92ff0
lucl1 <luchenglin@leadcoretech.com>
2018-03-20 11:22:29 +0800

    [modify] compare function

--- C++/qsort_deom/qsort_demo.cpp
+++ C++/qsort_deom/qsort_demo.cpp
@@ -2,9 +2,12 @@
 #include <stdio.h>
 
 int values[] = {40,10,100,90,20,25};
+
 int compare(const void *a, const void *b){
-    return (*(int*)a - *(int*)b);
+    return (*(int*)a - *(int*)b);   //由小到大
+    //return (*(int*)b - *(int*)a);   //由大到小
 }
+
 int main()
 {
     int n;
