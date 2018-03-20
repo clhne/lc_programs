1f8a18b2082cf9de949af901c62c7516f3710111
lucl1 <luchenglin@leadcoretech.com>
2018-03-20 10:03:10 +0800

    [add] qsort_c_demo

---
+++ C++/qsort_deom/qsort_demo.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int values[] = {40,10,100,90,20,25};
+int compare(const void *a, const void *b){
+    return (*(int*)a - *(int*)b);
+}
+int main()
+{
+    int n;
+    qsort (values, 6, sizeof(int), compare);
+    for (n = 0; n < 6; n++){
+        printf("%d ", values[n]);
+    }
+    return 0;
+}
---
+++ C++/qsort_deom/run.bat
@@ -0,0 +1,4 @@
+@echo off
+g++ -g qsort_demo.cpp -o qsort_demo
+qsort_demo.exe
+pause
