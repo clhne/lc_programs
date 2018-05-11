4664deb51bec516d28dfa4d85ddd02c0a3cfe4e1
lucl1 <luchenglin@leadcoretech.com>
2018-05-11 15:23:12 +0800

    [modify] add data

--- C++/kalman_demo/kalman_demo.cpp
+++ C++/kalman_demo/kalman_demo.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-float z[10] = {0.39, 0.50, 0.48, 0.29, 0.25, 0.32, 0.34, 0.48, 0.41, 0.45};
-float R = 0.1, p[10] = {1}, x[10] = {0},k[10] = {0};
+float z[23] = {0.39, 0.50, 0.48, 0.29, 0.25, 0.32, 0.34, 0.48, 0.41,               0.45, 0.32, 0.56, 0.69, 0.25, 0.64, 0.28, 0.61, 0.89,               0.46, 0.89, 0.84, 0.63, 0.27};
+float R = 0.1, p[23] = {1}, x[23] = {0},k[23] = {0};
 
 //TimeUpdate: prediction
 float time_update(int i){
@@ -19,7 +19,9 @@ float measurement_update(int j){
 }
 int main()
 {
-    for(int i = 0; i < 10; i++){
+	int N;
+	N = sizeof(z)/sizeof(z[0]);
+    for(int i = 0; i < N; i++){
         if(i == 0){
             k[0] = p[0]/(p[0] + R);
             x[0] += k[0]*(z[0]-x[0]);
@@ -28,7 +30,7 @@ int main()
         time_update(i);
         measurement_update(i);
     }
-    for(int j = 0; j < 10; j++){
+    for(int j = 0; j < N; j++){
         printf("x[%d]=%f, p[%d] = %f, k[%d]=%f\n",j,x[j],j,p[j],j,k[j]);
     }
     return 0;
