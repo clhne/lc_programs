f493d120f7e77bf12f44a97f1565464406f4c8d5
lucl1 <luchenglin@leadcoretech.com>
2018-06-19 13:59:29 +0800

    [modify] opt iir function

--- C++/iir_1ord_filter/iir_1ord_filter.cpp
+++ C++/iir_1ord_filter/iir_1ord_filter.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-int irr_1ord_filter(float *iir_data, float *input_data)
+void irr_1ord_filter(float *iir_data, float *input_data, short *index, short length)
 {
+	short idx = *index;
 	float coef = 0.55;
-	*iir_data = *input_data * (1 - coef) + *iir_data * coef;
-	return 1;
+	iir_data[idx] = input_data[idx] * (1 - coef) + iir_data[idx-1] * coef;
+	*index = (short)(idx + 1) % length;
 }
 int main(){
-	int i;
+	short i;
 	float coef = 0.55;
 	float iir_data[10] = {0};
 	float input_data[10] = {6, 5, 9, 10, 12, 13, 15, 15, 18, 20};
 	int length = sizeof(input_data)/sizeof(input_data[0]);
 	int is_init = 1;
+	short index = 1;
 	//write input_data to input_data.txt
 	ofstream myfile;
 	myfile.open("input_data.txt");
@@ -24,6 +26,8 @@ int main(){
 	
 	//irr_1ord_filter(irr_data, input_data), then write irr data to irr_data.txt
 	myfile.open("iir_data.txt");
+	//irr_1ord_filter(iir_data, input_data, &index, length);
+	
 	for (i = 1; i < 10; i++){
 		if(is_init){
 			iir_data[0]=input_data[0]*(1-coef) + iir_data[0]*coef;
@@ -31,7 +35,7 @@ int main(){
 		}
 		is_init = 0;
 		iir_data[i]=input_data[i]*(1-coef) + iir_data[i-1]*coef;
-		//irr_1ord_filter(&iir_data[i], &input_data[i]);
+		//irr_1ord_filter(iir_data, input_data, &index, length);
 		myfile<<iir_data[i]<<endl;
 	}
 	myfile.close();
