56dd662b1bc791a606a018fcc09137f872a8d544
lucl1 <luchenglin@leadcoretech.com>
2018-07-16 10:22:13 +0800

    [add] softmax python version

---
+++ python_3.5/softmax/softmax.py
@@ -0,0 +1,10 @@
+import numpy as np
+# softmax(x) = softmax(x+c)
+def softmax(x):
+    x = x - np.max(x)
+    exp_x = np.exp(x)
+    softmax_x = exp_x / np.sum(exp_x)
+    return softmax_x
+
+x = [1,4,2]
+print(softmax(x))
