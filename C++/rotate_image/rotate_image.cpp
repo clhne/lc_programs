4b9da694c5a1a59edb4e5dee2423b82897b61f44
lucl1 <luchenglin@leadcoretech.com>
2018-07-19 15:46:37 +0800

    [debug] opt

--- C++/rotate_image/rotate_image.cpp
+++ C++/rotate_image/rotate_image.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 #define  N (3)
 #define  M (3)
 
-void rotate_image(int **matrix, int n, int m){
-	int tmp;
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
-			tmp = matrix[j][i];
-			matrix[j][i] = matrix[j][m-i-1];
-			matrix[j][m-i-1] = tmp;
-		}
-	}
-}
-void rotate(vector<vector<int>>& matrix){
+//method 1
+void rotate1(vector<vector<int>>& matrix){
 	const int n = matrix.size();
 	for(int i = 0; i < n / 2; i++){
 		for(int j = i; j < n-1; j++){
@@ -27,26 +19,29 @@ void rotate(vector<vector<int>>& matrix){
 		}
 	}
 }
+
+//method 2 先转秩，再左右翻转
+void rotate2(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            swap(matrix[i][j], matrix[j][i]);
+        }
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
 int main(){
-	//int matrix[N][N] = {1,2,3;4,5,6;7,8,9};
-	/*
-	int **matrix = new int*[M];
-	for(int i = 0; i < M; i++){
-		matrix[i] = new int[N];
-	}
-	*/
-	vector<vector<int>> matrix(M,vector<int>(N));
-	//matrix[0][0] = 1;
+ 	vector<vector<int>> matrix(M,vector<int>(N));
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
 			matrix[i][j] = 3 * i + j + 1;
 		}
 	}
-	//rotate_image(matrix, N, M);
-	rotate(matrix);
+	//rotate1(matrix);
+	rotate2(matrix);
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
-			printf("%d, ", matrix[i][j]);
+			printf("%d  ", matrix[i][j]);
 		}
 		cout<<endl;
 	}
